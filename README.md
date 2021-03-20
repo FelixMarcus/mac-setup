ad09c6c2e95c1db6a1cc63019c0d3d41be2136dc
Felix Marcus Millne <fmillne@thoughtworks.com>
2021-03-20 15:04:29 +0000

    Add script to install directly from internet into ~/.mac-setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Install command:
+
+```
+#!/usr/bin/env bash
+bash <(curl -Ls https://github.com/FelixMarcus/mac-setup.git/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir -p /Users/$USER/.mac-setup
+git clone git@github.com:FelixMarcus/mac-setup.git /Users/$USER/.mac-setup
+cd /Users/$USER/.mac-setup
+./run_this.sh
+
