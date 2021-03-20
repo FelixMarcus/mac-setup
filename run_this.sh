d8f908273ef386f29c8371545e32906d3e6b2d32
Felix Marcus Millne <fmillne@thoughtworks.com>
2021-03-20 14:53:46 +0000

    Clean up run order to prevent failures due to left over directories

--- run_this.sh
+++ run_this.sh
@@ -1,24 +1,43 @@
-#!/usr/bin/env bash
+[[ -d $USER_DIR/.oh-my-zsh ]] && echo "Uninstalling old oh-my-zsh instance" && $USER_DIR/.oh-my-zsh/tools/uninstall.sh#!/usr/bin/env bash
 
-#Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+USER_DIR=/Users/$USER
+echo Detected User Directory of $USER_DIR
 
-#Install Powerlevel10k
+echo Cleaning up any left over state
+
+[[ -d $USER_DIR/.oh-my-zsh ]] && \
+	echo "Uninstalling old oh-my-zsh instance" && \
+	chmod +x $USER_DIR/.oh-my-zsh/tools/uninstall.sh && \
+	printf 'y\n' | $USER_DIR/.oh-my-zsh/tools/uninstall.sh
+
+rm -f $USER_DIR/.zshrc 2>/dev/null || echo No .zshrc file found 2>/dev/null
+unlink $USER_DIR/.zshrc 2>/dev/null || echo No .zshrc link found 2>/dev/null
+rm -f $USER_DIR/.p10k.zsh 2>/dev/null || echo no .p10k.zsh file found 2>/dev/null
+unlink $USER_DIR/.p10k.zsh 2>/dev/null || echo no .p10k.zsh link found 2>/dev/null
+unlink /usr/local/opt/openjdk/libexec/openjdk.jdk/openjdk.jdk 2>/dev/null || echo No openjdk link found 2>/dev/null
+
+echo '\n\n'
+
+echo Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo Symbolically link .zshrc to repo
+rm -f $USER_DIR/.zshrc # Remove template added by oh-my-zsh
+ln -s $PWD/.zshrc $USER_DIR/.zshrc
+ln -s $PWD/.p10k.zsh $USER_DIR/.p10k.zsh
+
+echo Install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-#Symbolically link .zshrc to repo
-ln -s $PWD/.zshrc /Users/$USER/.zshrc
-ln -s $PWD/.p10k.zsh /Users/$USER/.p10k.zsh
 
-#Install xcode CLT
+echo Install xcode CLT
 xcode-select --install
 
-# Install Homebrew, Taps And Casks
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo Install Homebrew Taps And Casks
 brew bundle
 
-# Link Brew Java as JVM so that it is recognised by /usr/local/java_home
+echo Link Brew Java as JVM so that it is recognised by /usr/local/java_home
 ln -s /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
-#Reload zsh using powerlevel config
-source ~/.zshrc
+echo Reload zsh using powerlevel config
+$PWD/configure_zsh
