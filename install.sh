24231db45aa3d063e6b3de45d724ef6296d7435b
Felix Marcus Millne <felix.millne@krakenflex.com>
2025-05-15 12:31:07 +0100

    set mac-setup source URL to SSH after cloning

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+
+echo "Creating hidden mac-setup directory (if it doesn't already exist)"
 mkdir -p /Users/$USER/.mac-setup
+
+echo "Cloning mac setup repo to ~/.mac-setup/ (as HTTPS)"
 git clone git@github.com:FelixMarcus/mac-setup.git /Users/$USER/.mac-setup
 cd /Users/$USER/.mac-setup
+
+echo "Set remote to SSH"
+git remote set-url origin git@github.com:FelixMarcus/mac-setup.git
+
+echo "Beginning mac setup"
 ./run_this.sh
 
